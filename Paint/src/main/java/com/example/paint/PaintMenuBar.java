ff8f0698cbd5a808292b15657be97be3ed679f86
Charlie <charlesmalach@gmail.com>
2022-09-07 11:16:24 -0500

    Updated code
    - fixed a lot and changed classes

---
+++ Paint/src/main/java/com/example/paint/PaintMenuBar.java
@@ -0,0 +1,198 @@
+package com.example.paint;
+
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.FileChooser;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+import static com.example.paint.Painters.*;
+
+public class PaintMenuBar extends MenuBar {
+
+    public PaintMenuBar() {
+        super();
+        //This section creates the MenuBar that hosts File, Help, Edit, Tools
+        System.out.println("Menu successfully launched.");
+        Menu File = new Menu("File");
+        Menu Help = new Menu("Help");
+        Menu Edit = new Menu("Edit");
+        Menu Options = new Menu("Options");
+
+        //This section adds the other main options to the menu bar
+        getMenus().add(File);
+        getMenus().add(Help); //TODO: add actual features under this menu item
+        getMenus().add(Edit); //TODO: add actual features under this menu item
+        getMenus().add(Options); //TODO: add actual features under this menu item
+
+
+        //'Open' menu item. Allows users to open a picture to the current project.
+        MenuItem Open = new MenuItem("Open");
+        Open.setMnemonicParsing(
+                true);
+        Open.setAccelerator(
+                new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN)); //sets hotkey CTRL + O --> Open Program
+        Open.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) { //Open File
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Open File");
+                fc.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("All Images", "*.*"),
+                        new FileChooser.ExtensionFilter("PNG Files", "*.png")
+                );
+                File file = fc.showOpenDialog(mainStage);
+                if (file != null) {
+                    try { // Presents and resizes the selected image on the canvas
+                        Image pic = new Image(file.toURI().toString());
+                        picture.setImage(pic);
+                        picture.setPreserveRatio(true);
+                        picture.setSmooth(true);
+                        picture.setCache(true);
+                        mainPicture.setMaxWidth(picture.getFitWidth());
+                        mainPicture.setMaxHeight(picture.getFitHeight());
+                        BufferedImage img = ImageIO.read(file);
+                        WritableImage image = SwingFXUtils.toFXImage(img, null);
+                        GraphicsContext gc = canvas.getGraphicsContext2D();
+                        gc.drawImage(image, picture.getFitWidth(), picture.getFitHeight());
+                        canvas.setWidth(picture.getFitWidth());
+                        System.out.println(file.getAbsolutePath() + " has been loaded.");
+                    } catch (Exception ex) {
+                        System.out.println("Error");
+                    }
+                    Saving = false;
+                }
+            }
+        }
+        );
+
+        //'Save' menu item. Allows users to save current project.
+        MenuItem Save = new MenuItem("Save"); //Creates menu option to save current user project
+        Save.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN)); //sets hotkey CTRL + S --> Save
+        Save.setMnemonicParsing(true);
+        Save.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Save File");
+                fc.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("All Images", "*.*")
+                );
+                File file = fc.showOpenDialog(mainStage);
+                System.out.println("Testing " + file.getAbsolutePath());
+                if (file.getPath() != null) { //this loop is broken, file is not currently set at the time of the loop running. it should be though.
+                    try {
+                        WritableImage writableImage = new WritableImage((int) mainPicture.getWidth(), (int) mainPicture.getHeight()); //this code has errors
+                        mainPicture.snapshot(null, writableImage);
+                        RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                        ImageIO.write(renderedImage, "png", file);
+                        System.out.println("File successfully saved to " + file.getAbsolutePath());
+                    } catch (IOException ex) {
+                        System.out.println("Error has occurred.");
+                    }
+                    Saving = true;
+                }
+            }
+        });
+
+        //'Save as' menu item. Allows users to save current project as a different file.
+        MenuItem SaveAs = new MenuItem("Save as..."); //Creates menu option to save current user project as different file
+        SaveAs.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN, KeyCombination.SHIFT_DOWN)); //sets hotkey CTRL + Shift + S --> Save
+        SaveAs.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Save As...");
+                fc.getExtensionFilters().addAll( //allows user to save image as any of the following extensions
+                        new FileChooser.ExtensionFilter("All Images", "*.*"),
+                        new FileChooser.ExtensionFilter("PNG Files", "*.png"),
+                        new FileChooser.ExtensionFilter("ICON Files", "*.png"),
+                        new FileChooser.ExtensionFilter("JPG Files", ".jpg")
+                );
+                File save = fc.showSaveDialog(mainStage);
+                if (save != null) {
+                    try {
+                        WritableImage writableImage = new WritableImage((int) mainPicture.getWidth(), (int) mainPicture.getHeight()); //this code has errors
+                        mainPicture.snapshot(null, writableImage);
+                        RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                        ImageIO.write(renderedImage, "png", save);
+                        file = save;
+                        saved_file = save;
+                        System.out.println("File successfully saved to " + file.getAbsolutePath());
+                    } catch (IOException ex) {
+                        System.out.println("Error has occurred.");
+                    }
+                    Saving = true;
+                }
+            }
+        }
+        );
+
+        //'Exit' menu item. Allows users to exit current project after prompting them to save.
+        SeparatorMenuItem separator = new SeparatorMenuItem();
+        MenuItem Exit = new MenuItem("Exit", null);
+        Exit.setMnemonicParsing(
+                true);
+        Exit.setAccelerator(
+                new KeyCodeCombination(KeyCode.ESCAPE)); //sets hotkey ESC --> Ends program
+        Exit.setOnAction(e -> {
+            if (Saving) {
+                Platform.exit();
+                System.exit(0);
+            } else {
+                Alert exit = new Alert(Alert.AlertType.CONFIRMATION);
+                exit.setTitle("File has NOT been Saved");
+                String text = "Would you like to save? (Click Cancel to close without saving.)";
+                exit.setContentText(text);
+                Optional<ButtonType> show = exit.showAndWait();
+
+                if ((show.isPresent()) && (show.get() == ButtonType.OK)) {
+                    try {
+                        FileChooser fc = new FileChooser();
+                        fc.setTitle("Save File");
+                        fc.getExtensionFilters().addAll(
+                                new FileChooser.ExtensionFilter("All Images", "*.*")
+                        );
+                        File file = fc.showOpenDialog(mainStage);
+                        WritableImage writableImage = new WritableImage((int) mainPicture.getWidth(), (int) mainPicture.getHeight());
+                        mainPicture.snapshot(null, writableImage);
+                        RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                        ImageIO.write(renderedImage, "png", file);
+                        System.out.println("File successfully saved to " + file.getAbsolutePath());
+                    } catch (IOException ex) {
+                        System.out.println("Error has occurred.");
+                    }
+
+                    //After project is saved, program will exit.
+                    Platform.exit();
+                    System.exit(0);
+                } else {
+                    //If Cancel button is clicked, program will just exit.
+                    Platform.exit();
+                    System.exit(0);
+                }
+            }
+        });
+        //This section adds all the File options to the menu bar
+        File.getItems().add(Open);
+        File.getItems().add(Save);
+        File.getItems().add(SaveAs);
+        File.getItems().add(separator);
+        File.getItems().add(Exit);
+    }
+}
--- Paint/src/main/java/com/example/paint/Painters.java
+++ Paint/src/main/java/com/example/paint/Painters.java
@@ -1,36 +1,18 @@
 package com.example.paint;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.GridPane;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
-import javax.imageio.ImageIO;
 
 /**
  *  Malachinski Pain(t) Application
- *  The Pain(t) application is a program that allows users to upload images.
+ *  The Pain(t) application is a program that currently allows users to upload, save and save as images. More features will come with future updates.
  *
  * @author Charlie Malachinski
  * @version 1.0.0
@@ -39,212 +21,40 @@ import javax.imageio.ImageIO;
  **/
 
 public class Painters extends Application {
-
-    ImageView picture = new ImageView(); //Creates a new ImageView object
-    File file; //Creates a local variable called 'file' for file management
-    File saved_file; //Creates a local variable called 'saved_file' for edited files
-    Boolean Saving = true; //Boolean to determine if file is saved
-    Canvas canvas = new Canvas(); //Creates new Canvas object
-
-    WritableImage wit = new WritableImage(200, 200); //Creates a new WritableImage object to construct images
-
-    GraphicsContext gc = canvas.getGraphicsContext2D(); //Creates a GraphicsContext to draw calls to a Canvas using a buffer
+    private final static String TITLE = "Malachinski - Pain(t)"; //Name of the application
+    private final static int windowLength = 1280; //Dictates the initial length of the application window
+    private final static int windowHeight = 720; //Dictates the initial width of the application window
+    public static ImageView picture = new ImageView(); //Creates a new ImageView object
+    public static File file; //Creates a local variable called 'file' for file management
+    public static File saved_file; //Creates a variable called 'saved_file' for edited files
+    public static  Boolean Saving = true; //Boolean to determine if file is saved
+    public static  Canvas canvas = new Canvas(); //Creates new Canvas object
+    public static GridPane main = new GridPane(); //Creates a new Grid Pane for the main application
+    public static GridPane mainCanvas = new GridPane(); //Creates Grid Pane for the Canvas
+    public static GridPane mainPicture = new GridPane(); //Creates Grid Pane for the Picture
+    public static PaintMenuBar menuBar; //Creates a MenuBar
+    public static Stage mainStage; //Creates the main Stage
 
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane main = new GridPane();
-        GridPane maincanvas = new GridPane();
-        GridPane mainpicture = new GridPane();
-
-        //This section creates the Pain(t) scene
-        Scene scene = new Scene(main, 1000, 900);
-        stage.setTitle("Malachinski - Pain(t)");
-//        Image icon = new Image("../src/icons/icon.png");          icon is not working
-//        stage.getIcons().add(icon);
-        stage.setScene(scene);
-        stage.show();
-
-        //This section creates the MenuBar that hosts File, Help, Edit, Tools
-        MenuBar menuBar = new MenuBar();
+        Painters.mainStage = stage; //creates the new stage for the application
+        Scene scene = new Scene(main, windowLength, windowHeight); //creates a new scene with the main Grid Pane and the desired application window size.
+        menuBar = new PaintMenuBar(); //Creates a new menu bar
         menuBar.prefWidthProperty().bind(stage.widthProperty()); //extends width of entire program
-        Menu File = new Menu("File");
-        Menu Help = new Menu("Help");
-        Menu Edit = new Menu("Edit");
-        Menu Tool = new Menu("Tool");
+        mainPicture.add(picture, 0, 0); //Adds the image to the canvas at the row 0, column 0.
+        mainPicture.setAlignment(Pos.CENTER); //Aligns the picture to the center of the canvas
 
-        //Creates menu option to open an image on user project
-        MenuItem Open = new MenuItem("Open");
-        Open.setMnemonicParsing(
-                true);
-        Open.setAccelerator(
-                new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
-        Open.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) { //Open File
-                FileChooser fc = new FileChooser();
-                fc.setTitle("Open File");
-                fc.getExtensionFilters().addAll(
-                        new ExtensionFilter("All Images", "*.*"),
-                        new ExtensionFilter("PNG Files", "*.png")
-                );
-                File file = fc.showOpenDialog(stage);
-                if (file != null) {
-                    try { // Presents and resizes the selected image on the canvas
-                        Image pic = new Image(file.toURI().toString());
-                        picture.setImage(pic);
-                        picture.setPreserveRatio(true);
-                        picture.setFitWidth(canvas.getWidth());
-                        picture.setFitHeight(canvas.getHeight());
-                        picture.setSmooth(true);
-                        picture.setCache(true);
-                        BufferedImage img = ImageIO.read(file);
-                        WritableImage image = SwingFXUtils.toFXImage(img, null);
-                        GraphicsContext gc = canvas.getGraphicsContext2D();
-                        gc.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight());
-                        int imageHeight = (int) image.getHeight();
-                        int imageWidth = (int) image.getWidth();
-                        System.out.println(file.getAbsolutePath());
-                    } catch (Exception ex) {
-                        System.out.println("Error");
-                    }
-                    Saving = false;
-                }
-            }
-        }
-        );
 
-        MenuItem Save = new MenuItem("Save"); //Creates menu option to save current user project
-        Save.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN)); //sets hotkey CTRL + S --> Save
-        Save.setMnemonicParsing(true);
+        //organizes canvas layout
+        main.addRow(1, menuBar); //adds the menu bar to the scene
+        main.addRow(2, mainCanvas); //adds the main canvas to the scene
+        main.addRow(3, mainPicture); //adds the main picture to the scene
 
-        Save.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                FileChooser fc = new FileChooser();
-                fc.setTitle("Open File");
-                fc.getExtensionFilters().addAll(
-                        new ExtensionFilter("All Images", "*.*"),
-                        new ExtensionFilter("PNG Files", "*.png")
-                );
-                File file = fc.showOpenDialog(stage);
-                System.out.println(file.getAbsolutePath());
-                if (file != null) { //this loop is broken, file is not currently set at the time of the loop running. it should be though.
-                    try {
-                        System.out.println("looking for path...");
-                        System.out.println(file.getAbsolutePath());
-                        WritableImage writableImage = new WritableImage((int) mainpicture.getWidth(), (int) mainpicture.getHeight()); //this code has errors
-                        mainpicture.snapshot(null, writableImage);
-                        RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
-                        ImageIO.write(renderedImage, "png", file);
-                        System.out.println(file.getAbsolutePath());
-                        System.out.println("File successfully saved");
-                    } catch (IOException ex) {
-                        System.out.println("Error has occurred.");
-                    }
-                    Saving = true;
-                }
-            }
-        });
-
-        MenuItem SaveAs = new MenuItem("Save as..."); //Creates menu option to save current user project as different file
-        SaveAs.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN,KeyCombination.SHIFT_DOWN)); //sets hotkey CTRL + Shift + S --> Save
-        SaveAs.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                FileChooser fc = new FileChooser();
-                fc.setTitle("Save As...");
-                fc.getExtensionFilters().addAll( //allows user to save image as any of the following extensions
-                        new FileChooser.ExtensionFilter("All Images", "*.*"),
-                        new FileChooser.ExtensionFilter("PNG Files", "*.png"),
-                        new FileChooser.ExtensionFilter("ICON Files", "*.png"),
-                        new FileChooser.ExtensionFilter("JPG Files", ".jpg")
-                );
-                File save = fc.showSaveDialog(stage);
-                if (save != null) {
-                    try {
-                        System.out.println(mainpicture.getWidth() + mainpicture.getHeight());
-                        WritableImage writableImage = new WritableImage((int) mainpicture.getWidth(), (int) mainpicture.getHeight()); //this code has errors
-                        System.out.println(canvas.getWidth() + canvas.getHeight());
-                        mainpicture.snapshot(null, writableImage);
-                        RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
-                        ImageIO.write(renderedImage, "png", save);
-                        file = save;
-                        saved_file = save;
-                    } catch (IOException ex) {
-                        System.out.println("Error has occurred.");
-                    }
-                    Saving = true;
-                }
-            }
-        }
-        );
-
-        SeparatorMenuItem separator = new SeparatorMenuItem();
-        MenuItem Exit = new MenuItem("Exit", null);
-        Exit.setMnemonicParsing(
-                true);
-        // Ctrl + X
-        Exit.setAccelerator(
-                new KeyCodeCombination(KeyCode.ESCAPE));
-        Exit.setOnAction(e -> {
-            if (Saving) {
-                Platform.exit();
-                System.exit(0);
-            } else {
-                Alert exit = new Alert(Alert.AlertType.CONFIRMATION);
-                exit.setTitle("File has not been Saved");
-                String text = "Would you like to save? (Click Cancel to close project.)";
-                exit.setContentText(text);
-                Optional<ButtonType> show = exit.showAndWait();
-
-                if ((show.isPresent()) && (show.get() == ButtonType.OK)) {
-                    try {
-                        WritableImage writableImage = new WritableImage((int) mainpicture.getWidth(), (int) mainpicture.getHeight()); //this code has errors
-                        mainpicture.snapshot(null, writableImage);
-                        RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
-                        ImageIO.write(renderedImage, "png", saved_file); //this sometimes breaks?
-                    } catch (IOException ex) {
-                        System.out.println("Error has occurred.");
-                    }
-
-                    //exits program
-                    Platform.exit();
-                    System.exit(0);
-                } else {
-                    //if cancel button is clicked
-                    Platform.exit();
-                    System.exit(0);
-                }
-            }
-        });
-
-        //This section adds all the File options to the menu bar
-        menuBar.getMenus().add(File);
-        File.getItems().add(Open);
-        File.getItems().add(Save);
-        File.getItems().add(SaveAs);
-        File.getItems().add(separator);
-        File.getItems().add(Exit);
-
-        //This section adds the other main options to the menu bar
-        menuBar.getMenus().add(Help);
-        menuBar.getMenus().add(Edit);
-        menuBar.getMenus().add(Tool);
-
-        main.setHgap(0);
-        main.setVgap(-5);
-        main.addRow(1, menuBar);
-        main.addRow(2, maincanvas);
-        main.addRow(3, mainpicture);
-
-        maincanvas.setHgap(100);
-        maincanvas.setVgap(20);
-
-        mainpicture.setHgap(100);
-        mainpicture.setVgap(20);
-        mainpicture.add(picture, 0, 1);
-        mainpicture.minHeight(400);
-        mainpicture.minWidth(400);
+        //organizes stage
+        stage.setTitle(TITLE); //sets the title
+        stage.setScene(scene); //sets the scene
+        stage.show(); //shows the scene on screen
     }
-
-
 
     public static void main(String[] args) {
         launch();
