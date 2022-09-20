6a5e3bd88af907d70f67aa502d7bfd375dca3144
Charlie <charlesmalach@gmail.com>
2022-09-20 11:18:18 -0500

    Update PaintDraw.java

--- Paint/src/main/java/com/example/paint/PaintDraw.java
+++ Paint/src/main/java/com/example/paint/PaintDraw.java
@@ -14,39 +14,28 @@ import java.io.File;
 public class PaintDraw extends Canvas {
 
     private GraphicsContext gc;
-    private boolean fillShape;
 
     public PaintDraw() {
         super();
-        this.fillShape = false;
         this.gc = this.getGraphicsContext2D();
         this.gc.setLineCap(StrokeLineCap.ROUND);
     }
 
     public void drawRect(double x1, double y1, double x2, double y2) {
-        double x = (x1 < x2 ? x1 : x2); //set x to the smaller of the two values to map to bottom left
-        double y = (y1 < y2 ? y1 : y2); //
+        double x = (Math.min(x1, x2)); //set x to the smaller of the two values to map to bottom left
+        double y = (Math.min(y1, y2));
         double w = Math.abs(x1 - x2);   //abs val of the two x's = length of x
         double h = Math.abs(y1 - y2);
-        if (this.getFillShape())
-            this.gc.fillRect(x, y, w, h);
         this.gc.strokeRect(x, y, w, h);
     }
 
-    public void drawSquare(double x1, double y1, double x2, double y2) {
-        final double ANGLE_45 = Math.PI / 4.0;    //pi/4 is a 45 degree angle, which makes it square instead of a diamond
-        final int SIDES = 4;
-        double[] xPoints = new double[SIDES];   //4 is the number of sides a square has
-        double[] yPoints = new double[SIDES];
-        double radius = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
-        //try and figure out how to fix later
-        for (int i = 0; i < SIDES; i++) {
-            xPoints[i] = x1 + (radius * Math.cos(((2 * Math.PI * i) / 4) + ANGLE_45));
-            yPoints[i] = y1 + (radius * Math.sin(((2 * Math.PI * i) / 4) + ANGLE_45));
-        }
-        if (this.getFillShape())
-            this.gc.fillPolygon(xPoints, yPoints, SIDES);
-        this.gc.strokePolygon(xPoints, yPoints, SIDES);
+    public void drawSquare(double x1, double y1, double x2, double y2)
+    {
+        double x = (Math.min(x1, x2)); //set x to the smaller of the two values to map to bottom left
+        double y = (Math.min(y1, y2));
+        double w = Math.abs(x1 - x2);   //abs val of the two x's = length of x
+        double h = Math.abs(x1 - x2);
+        this.gc.strokeRect(x, y, w, h);
     }
 
     public void drawEllipse(double x1, double y1, double x2, double y2) {
@@ -123,10 +112,6 @@ public class PaintDraw extends Canvas {
             Image img = new Image(file.toURI().toString());
             this.drawImage(img);
         }
-    }
-
-    public boolean getFillShape() {
-        return this.fillShape;
     }
 
     public void drawImageAt(Image im, double x, double y) {
