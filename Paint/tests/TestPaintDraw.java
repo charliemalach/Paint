b09839f5eaf5b37581043086d114536d2bc935fa
Charlie <charlesmalach@gmail.com>
2022-10-02 13:05:50 -0500

    created unit testing class

---
+++ Paint/tests/TestPaintDraw.java
@@ -0,0 +1,20 @@
+package com.example.paint;
+
+import javafx.scene.paint.Color;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPaintDraw {
+
+    public TestPaintDraw(){
+
+    }
+
+    @Before
+
+}
+
