ac29217d2cb591d91bc24699eabd8f1ece2abf87
Charlie <charlesmalach@gmail.com>
2022-10-05 10:41:52 -0500

    unit tests working

--- Paint/test/com/example/paint/PaintDrawTest.java
+++ Paint/test/com/example/paint/PaintDrawTest.java
@@ -1,7 +1,11 @@
 package com.example.paint;
 
 import javafx.scene.paint.Color;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -12,25 +16,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PaintDrawTest {
 
+    public PaintDrawTest(){
+
+    }
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Tests the setLineWidth() method to determine if it works properly. 
+     */
     @Test
     public void testSetLineWidth() {
-        System.out.println("setLineWidth");
+        System.out.println("setLineWidth test");
         double width = 1.0;
         PaintDraw test = new PaintDraw();
         test.setLineWidth(width);
         assertEquals(1.0, test.getLineWidth(), 0.1);
+        System.out.println("setLineWidth was successful.");
     }
 
+    /**
+     * Tests the setLineColor() method to determine if it works properly.
+     */
     @Test
     public void testSetLineColor() {
-        System.out.println("setLineColor");
+        System.out.println("setLineColor test");
         Color color = Color.BLACK;
         PaintDraw test = new PaintDraw();
         test.setLineColor(color);
         assertEquals(color, test.getLineColor());
-
+        System.out.println("setLineColor was successful.");
     }
 
+    /**
+     * Tests the setFillColor() method to determine if it works properly.
+     */
     @Test
     public void testSetFillColor() {
         System.out.println("setFillColor");
@@ -38,5 +71,6 @@ class PaintDrawTest {
         PaintDraw test = new PaintDraw();
         test.setFillColor(color);
         assertEquals(color, test.getFillColor());
+        System.out.println("setFillColor was successful.");
     }
 }
